e9a968ecdfa56ea90bdaa888c9c7bcf8c7032afa
PavelGenitsoy <pavlogenizoy@gmail.com>
2022-04-21 02:52:28 +0300

    added preprocessing with generating masks

---
+++ preproccesing_dataset_and_analyzing_results/preprocessing.py
@@ -0,0 +1,33 @@
+from utils_local import *
+from torchvision import transforms
+
+
+def main():
+    cnn_filtered_photos_folder = Path('data/input/cnn_filtered_standardized_with_normalized_105_pins')
+    hog_filtered_photos_folder = Path('data/input/filtered_standardized_with_normalized_105_pins')
+
+    mask_cnn_filtered_photos_folder = Path('data/input/masks_cnn_filtered_standardized_with_normalized_105_pins')
+    mask_hog_filtered_photos_folder = Path('data/input/masks_hog_filtered_standardized_with_normalized_105_pins')
+
+    triple = (
+        ('cnn', cnn_filtered_photos_folder, mask_cnn_filtered_photos_folder),
+        ('hog', hog_filtered_photos_folder, mask_hog_filtered_photos_folder)
+    )
+
+    tqdm_batch_inputs = tqdm(triple, total=len(triple))
+    for type_model, input_photos_folder, output_photos_folder in tqdm_batch_inputs:
+        filtered_dataset = ImageFolder(input_photos_folder.as_posix(), transform=transforms.ToTensor())
+
+        tqdm_batch_inputs.set_description(f'Filtered by {type_model}')
+
+        params = {
+            'classes': filtered_dataset.classes,
+            'output_photos_folder': output_photos_folder,
+            'type_model': type_model
+        }
+
+        parallelize_filter_images(processing_photos, np.array(filtered_dataset.imgs), params)
+
+
+if __name__ == '__main__':
+    main()
---
+++ preproccesing_dataset_and_analyzing_results/utils_local.py
@@ -0,0 +1,76 @@
+import face_recognition as fr
+
+from utils import *
+
+from PIL import ImageDraw
+from functools import partial
+from multiprocessing import cpu_count, Pool, current_process
+
+from typing import Callable
+
+
+def parallelize_filter_images(func: Callable, data: np.ndarray, kwargs: dict = {}, n_jobs=None):
+    if n_jobs is None:
+        cores = cpu_count()
+    else:
+        cores = n_jobs
+    available_data = data.shape[0]
+    if available_data < cores:
+        cores = available_data
+    data_split = np.array_split(data, cores)
+    pool = Pool(cores)
+    pool.map(partial(func, **kwargs), data_split)
+    pool.close()
+    pool.join()
+
+
+def creates_folders(folders_names: list, default_folder: Path):
+    for i in folders_names:
+        (default_folder / Path(i)).mkdir(parents=True, exist_ok=True)
+    print('\nFolders created!!')
+
+
+def save_image(dataset: ImageFolder, default_folder: Path):
+    for idx, (p, lbl) in tqdm(enumerate(dataset.imgs), total=len(dataset.imgs), desc=f'Progress bar...'):
+        arr = get_numpy_from_tenzor(dataset[idx][0])
+
+        image = Image.fromarray((arr * 255).astype(np.uint8)).convert('RGB')
+        image.save(f"{default_folder}/{dataset.classes[lbl]}/{Path(p).name}")
+
+    del arr
+    collect()
+    print('\nStandardised images saved!!')
+
+
+def generate_mask(path: Path, model: str = 'hog') -> Image:
+    image = fr.load_image_file(path.as_posix())
+
+    face_landmarks_list = fr.face_landmarks(
+        image,
+        face_locations=fr.face_locations(image, number_of_times_to_upsample=0, model=model),
+        model='custom'
+    )
+
+    if face_landmarks_list:
+        pil_image = Image.fromarray(image)
+        d = ImageDraw.Draw(pil_image)
+        d.polygon((face_landmarks_list[0]['mask_55']), fill='#00bfff')
+        return pil_image
+    else:
+        return path
+
+
+def processing_photos(dataset: np.ndarray, output_photos_folder: Path, classes: list, type_model: str):
+    tqdm_batch = tqdm(dataset, total=dataset.shape[0], desc=f'Processing ({current_process().name})...')
+    for path_to_file, lbl in tqdm_batch:
+        path_to_file = Path(path_to_file)
+        file_name, label = path_to_file.name, Path(classes[eval(lbl)])
+
+        (output_photos_folder / label).mkdir(parents=True, exist_ok=True)
+
+        pil_image = generate_mask(path_to_file, model=type_model)
+        if isinstance(pil_image, Path):
+            with open(f'data/skipped_images_for_{type_model}_by_{current_process().name}.txt', 'a') as f:
+                print(pil_image, file=f)
+        else:
+            pil_image.save(output_photos_folder / label / file_name)
