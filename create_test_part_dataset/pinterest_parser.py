f18bf92a36ecd9c5b3c243f0ccd5239fd7243a93
PavelGenitsoy <pavlogenizoy@gmail.com>
2022-04-27 18:45:56 +0300

    was implemented another logic for parallel downloading photos

--- create_test_part_dataset/pinterest_parser.py
+++ create_test_part_dataset/pinterest_parser.py
@@ -1,7 +1,7 @@
 import time
+import numpy as np
 import argparse
-
-from pathlib import Path
+import sys
 
 from webdriver_manager.chrome import ChromeDriverManager
 
@@ -10,136 +10,82 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
+from typing import Tuple
+from pathlib import Path
 from tqdm import tqdm
-from threading import Thread, Lock
-from queue import Queue
 from cv2 import imwrite, imdecode, resize
 from urllib.request import urlopen
-from numpy import asarray
 
-lock = Lock()
-
-
-class bingo:
-    DONE = False
+sys.path.append(f'{sys.path[0]}/..')
+from utils import parallelize
 
 
-class Ilister:
-    ilist = Queue()
-    copy_l = []
-    leni = 0
-    fn = None
-
-    def __init__(self, nmb: int, fn: str):
-        Ilister.fn = fn
-        options = Options()
-        options.add_argument("--start-maximized")
-        options.add_argument("--headless")
-        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-        link = f'https://tr.pinterest.com/search/pins/?q={fn}'
-        driver.get(link)
-
-        q = 0
-        bfn = 0
-        samen = 0
-        while True:
-            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-            if len(driver.find_elements(By.TAG_NAME, 'img')) >= nmb:
-                break
-
-            if len(driver.find_elements(By.TAG_NAME, 'img')) <= 5:
-                q += 1
-                if q == 50:
-                    print(f"Error! I can't find enough image!(Limit is 5, and there is no 5 image in this page! ) ")
-                    break
-
-            if bfn == len(driver.find_elements(By.TAG_NAME, 'img')):
-                samen += 1
-                if samen == 500:
-                    print(f"There is no enough image, breaking down... I'm gonna download {bfn} image")
-                    nmb = bfn
-                    break
-
-            bfn = len(driver.find_elements(By.TAG_NAME, 'img'))
-
-            time.sleep(0.5)
-
-        images = driver.find_elements(By.TAG_NAME, 'img')
-
-        Path(f'data/parsed_from_pinterest/pins_{fn}').mkdir(parents=True, exist_ok=True)
-
-        q = 0
-        for image in images:
-            if q >= nmb:
-                break
-            item = (image.get_attribute('src').replace("236x", "564x"), q)
-            Ilister.copy_l.append(item)
-            Ilister.ilist.put(item)
-            q += 1
-        Ilister.leni = Ilister.ilist.qsize()
-
-        driver.close()
+parsed_photos = Path('data/parsed_from_pinterest')
 
 
-class Downloader:
-    time1 = None
+def get_data(url: str, nmb: int) -> np.ndarray:
+    options = Options()
+    options.add_argument("--start-maximized")
+    options.add_argument("--headless")
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    driver.get(url)
+
+    times = 0
     q = 0
+    bfn = 0
+    samen = 0
+    while True:
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        if len(driver.find_elements(By.TAG_NAME, 'img')) >= nmb:
+            break
 
-    def __init__(self, reshapep: bool, shape: tuple, pbar, iq=20):
-        self.fn = Ilister.fn
-        self.reshapep = reshapep
-        self.shape = shape
-        self.pbar = pbar
+        if len(driver.find_elements(By.TAG_NAME, 'img')) <= 5:
+            q += 1
+            if q == 50:
+                print(f"Error! I can't find enough image!(Limit is 5, and there is no 5 image in this page! ) ")
+                break
 
-        Downloader.time1 = time.time()
-        Downloader.q = 0
-        for i in range(5):
-            t = Thread(target=self.checker)
-            t.daemon = True
-            t.start()
+        if bfn == len(driver.find_elements(By.TAG_NAME, 'img')):
+            samen += 1
+            if samen == 500:
+                print(f"There is no enough image, breaking down... I'm gonna download {bfn} image")
+                nmb = bfn
+                break
+        times += 1
+        bfn = len(driver.find_elements(By.TAG_NAME, 'img'))
 
-        self.fpi = (100 - iq) / int(Ilister.ilist.qsize())
-        self.proc = iq
-        Ilister.ilist.join()
-        del Ilister.ilist
-        Ilister.ilist = Queue()
+        time.sleep(0.5)
 
-    def downloads(self, image):
-        number = image[1]
-        image = image[0]
-        img = imdecode(asarray(bytearray(urlopen(image).read()), dtype="uint8"), -1)
+    images = driver.find_elements(By.TAG_NAME, 'img')
 
-        if img is not None:
-            if self.reshapep:
-                img = resize(img, self.shape)
+    print(f'\n======> Was scrolled {times} times and find {len(images)} images!')
 
-            imwrite(f"data/parsed_from_pinterest/pins_{self.fn}/{self.fn}{number}.jpg", img)
+    assert len(images) > 4, \
+        f'########### ERROR: Too small count of parsed images = {len(images)}. Need at least 5 ###########'
 
-        self.proc += self.fpi
-        if self.pbar is not None:
-            self.pbar.setValue(int(self.proc))
+    res = []
+    for idx, image in enumerate(images):
+        if idx >= nmb:
+            break
+        res.append((image.get_attribute('src'), str(idx)))
 
-        Downloader.q += 1
+    driver.close()
 
-    def checker(self):
-        while True:
-            image = Ilister.ilist.get()
-            self.downloads(image)
-            Ilister.ilist.task_done()
+    assert len(res) == nmb, \
+        f'########### ERROR: Incorrect len of parsed data, got {len(res)} but need {nmb} ###########'
+
+    return np.array(res)
 
 
-def launching(keyword: str, numberl: int, reshape: bool, shape: tuple = (256, 256), pbar=None):
-    if pbar is not None:
-        pbar.setValue(5)
+def downloads(data_ar: np.ndarray, folder_name: str, reshape: bool, shape: Tuple[int, int]):
+    for data in data_ar.tolist():
+        image = imdecode(np.asarray(bytearray(urlopen(data[0]).read()), dtype="uint8"), -1)
 
-    Ilister(numberl, keyword)
+        if image is not None:
+            if reshape:
+                image = resize(image, shape)
 
-    if Ilister.leni <= 5:
-        print(f"I can't find any image about {keyword}")
-    else:
-        Downloader(reshape, shape, pbar)
-
-    bingo.DONE = Thread
+            imwrite(f"{parsed_photos}/pins_{folder_name}/{folder_name}_{eval(data[1]):03d}.jpg", image)
 
 
 def get_parser() -> argparse.ArgumentParser:
@@ -159,13 +105,13 @@ def get_parser() -> argparse.ArgumentParser:
 def main() -> None:
     args = get_parser().parse_args()
 
-    reshaped = False
-    new_shape = tuple()
+    reshape = False
+    new_shape = (256, 256)
 
     if args.shape:
         assert len(args.shape) == 2, \
             f'########### ERROR: Need only 2 values of shape images (width, height). But get: {args.input} ###########'
-        reshaped = True
+        reshape = True
         new_shape = args.shape
 
     if 'all' in args.keyword:
@@ -173,13 +119,28 @@ def main() -> None:
     else:
         keywords = args.keyword
 
+    excess_keywords = list()
+    if parsed_photos.is_dir():
+        excess_keywords = sorted([p.name.split('_')[-1] for p in parsed_photos.glob('*')])
+
     tq_batch = tqdm(keywords, total=len(keywords))
     for keyword in tq_batch:
+        if keyword in excess_keywords:
+            continue
+
         tq_batch.set_description(f'Parsing...[{keyword}]')
 
+        Path(f'{parsed_photos}/pins_{keyword}').mkdir(parents=True, exist_ok=True)
+        link = f'https://tr.pinterest.com/search/pins/?q={keyword}'
+
         try:
-            launching(keyword, args.quantity, reshaped, new_shape)
-            print(f'=========> Parsing {keyword} is done!! <=========')
+            start = time.time()
+            data_array = get_data(link, args.quantity)
+            print(f'=========> Array which contains url and number of images for {keyword} was done !'
+                  f'(wasted time = {time.time() - start:.2f} sec) <=========')
+
+            parallelize(downloads, data_array, {'folder_name': keyword, 'reshape': reshape, 'shape': new_shape})
+            print(f'=========> Parsing {keyword} is done!! <=========\n')
         except Exception as error:
             with open('data/errors_in_parsing.txt', mode='a') as f:
                 print(f'{keyword}: {error}\n', file=f)
