b36e0ddae616af3483973ad635adc7eeb4fb2aa1
PavelGenitsoy <pavlogenizoy@gmail.com>
2022-05-09 01:34:52 +0300

    was added parts for preprocessing datasets

--- filters/filter_main.py
+++ filters/filter_main.py
@@ -1,12 +1,16 @@
 import sys
 import os
+import json
+import random
+import shutil
+import face_recognition as fr
 
 from PIL import Image
 
 sys.path.append(f'{sys.path[0]}/..')
 from utils import (creates_folders, parallelize, parallelize_upd, save_image, standardization)
 from utils import (RS, MEAN, STD, List, Tuple)
-from utils import (torch, np, Path, tqdm, fr, current_process, ImageFolder, time)
+from utils import (torch, np, Path, tqdm, current_process, ImageFolder, time)
 
 
 torch.manual_seed(RS)
@@ -82,10 +86,119 @@ def launch_get_faces(root: str, input_path: Path, output: Path, labels: list, mo
     )
 
 
+def find_incorrect_photos(f_list: list, root: Path, shape: Tuple[int, int]) -> None:
+    d_main_results = dict()
+    face_location = [(0, shape[0], shape[1], 0)]
+    for folder_path in tqdm(f_list, total=len(f_list), desc=f'Processing folders ({current_process().name})...'):
+        if not folder_path.is_dir():
+            continue
+
+        d_results = dict()
+        path_to_files = list(folder_path.glob('*'))
+
+        for idx_outer, image_path in enumerate(path_to_files):
+            if not image_path.is_file():
+                continue
+
+            image_main = fr.face_encodings(fr.load_image_file(image_path), face_location, 3, 'large')[0]
+
+            distances = []
+            for idx_inner, path_to_file in enumerate(path_to_files):
+                if not path_to_file.is_file():
+                    continue
+
+                if idx_outer != idx_inner:
+                    encoding = fr.face_encodings(fr.load_image_file(path_to_file), face_location, 3, 'large')[0]
+                    distance = fr.face_distance([encoding], image_main)[0]
+
+                    if 0.23 < distance <= 0.25:
+                        with open(f'{root}/similar_images_{current_process().name}.txt', 'a') as f:
+                            print(f'{image_path.stem} similar with {path_to_file.stem}: {distance}', file=f)
+                        continue
+                    elif distance <= 0.23:
+                        path_to_file.unlink()
+                        try:
+                            del d_results[path_to_file.name]
+                        except:
+                            pass
+                        continue
+
+                    distances.append(distance)
+
+            d_results[image_path.name] = distances
+        d_main_results[folder_path.as_posix()] = d_results
+    with open(f'{root}/data_{current_process().name}.json', 'w') as fp:
+        json.dump(d_main_results, fp)
+
+
+def find_incorrect_photos_v2(f_list: list, root: Path, shape: Tuple[int, int]) -> None:
+    d_main_results = dict()
+    face_location = [(0, shape[0], shape[1], 0)]
+    for folder_path in tqdm(f_list, total=len(f_list), desc=f'Processing folders ({current_process().name})...'):
+        if not folder_path.is_dir():
+            continue
+
+        d_results = dict()
+        path_to_files = list(folder_path.glob('*'))
+
+        etalon_prefix = Path(f'data/datasets_for_testing_part/10_images_by_class_valid/{folder_path.name}')
+        etalon_images = list(etalon_prefix.glob('*'))
+
+        etalon_encodings = \
+            [fr.face_encodings(fr.load_image_file(path), face_location, 3, 'large')[0] for path in etalon_images]
+
+        for idx_outer, image_path in enumerate(path_to_files):
+            if image_path.name in list(map(lambda x: x.name, etalon_images)):
+                continue
+
+            encoding = fr.face_encodings(fr.load_image_file(image_path), face_location, 3, 'large')[0]
+            distance = fr.face_distance(etalon_encodings, encoding)
+
+            checks_etalon = [True if val <= 0.25 else False for val in list(distance)]
+            if any(checks_etalon):
+                with open(f'{root}/VERY_similar_images_{current_process().name}.txt', 'a') as f:
+                    print(f'{image_path.stem} very similar with {checks_etalon.count(True)} of etalon images: '
+                          f'{np.array([i.name for i in etalon_images])[checks_etalon]}', file=f)
+
+            distance = np.mean(distance)
+
+            if 0.23 < distance <= 0.25:
+                with open(f'{root}/similar_images_{current_process().name}.txt', 'a') as f:
+                    print(f'{image_path.stem} similar with 10 etalon images: {distance}', file=f)
+                continue
+            # elif distance <= 0.23:
+            #     image_path.unlink()
+            #     try:
+            #         del d_results[path_to_file.name]
+            #     except:
+            #         pass
+            #     with open(f'{root}/VERY_similar_images_{current_process().name}.txt', 'a') as f:
+            #         print(f'{image_path.stem} similar with {image_path.stem}: {distance}', file=f)
+            #     continue
+
+            d_results[image_path.name] = distance
+        d_main_results[folder_path.as_posix()] = d_results
+    with open(f'{root}/data_{current_process().name}.json', 'w') as fp:
+        json.dump(d_main_results, fp)
+
+
+def find_duplicate_in_records(data: List[set]) -> dict:
+    d_duplicates = dict()
+    for idx, pair in enumerate(data):
+        mask_duplicates = [pair == tmp if idx != idx_inner else False for idx_inner, tmp in enumerate(data)]
+        index_of_duplicate = np.where(mask_duplicates)[0]
+        if index_of_duplicate.size:
+            pair = list(pair)
+            if pair[0] in d_duplicates.keys() or pair[1] in d_duplicates.keys():
+                continue
+            d_duplicates[pair[0]] = index_of_duplicate[0]
+    return d_duplicates
+
+
 # standardise dataset and save
-def stage_0_standardise(root_prefix: str):
-    folder_with_all_photos = Path(f'{root_prefix}/parsed_from_pinterest')
-    standard_folder_with_all_photos = Path(f'{root_prefix}/standard_parsed_from_pinterest')
+def stage_3_standardise(root_prefix: str):
+    folder_with_all_photos = Path(f'{root_prefix}/all_unique_parsed_images')
+    standard_folder_with_all_photos = Path(f'{root_prefix}/standard_all_unique_parsed_images')
 
     assert folder_with_all_photos.is_dir(), '########### ERROR: Input folder isn\'t exists! ###########'
 
@@ -116,13 +229,115 @@ def stage_1_filter_and_faces(root_prefix: str, model: str, shape: Tuple[int, int
     )
 
 
+def stage_2_find_duplicates(root_prefix: Path, shape: Tuple[int, int] = (256, 256)):
+    # p = Path('data/datasets_for_testing_part/resized_one_face_filter_hog_parsed_from_pinterest')
+    p = Path('data/datasets_for_testing_part/after_duplicates_resized_one_face_filter_hog_parsed_from_pinterest')
+    root_prefix = root_prefix / Path('logs_compare_etalon')
+    root_prefix.mkdir(parents=True, exist_ok=True)
+
+    before = list(p.glob('*'))
+    # parallelize(find_incorrect_photos, np.array(before), {'shape': shape, 'root': root_prefix})
+    parallelize(find_incorrect_photos_v2, np.array(before), {'shape': shape, 'root': root_prefix})
+
+    with open(f'{root_prefix}/similar_images.txt', 'w') as outfile:
+        for filename in root_prefix.glob('*.txt'):
+            with open(filename) as infile:
+                outfile.write(infile.read())
+
+    with open(f'{root_prefix}/similar_images_upd.txt', 'w') as f_out:
+        with open(f'{root_prefix}/similar_images.txt') as f_in:
+            main_content = f_in.readlines()
+            tmp = []
+            content = list(map(lambda x: x.rstrip().split(':')[0], main_content.copy()))
+            for line in content:
+                first_part = line.split('similar with')[0].rstrip()
+                second_part = line.split('similar with')[1].lstrip()
+                tmp.append({first_part, second_part})
+
+            for i in sorted(find_duplicate_in_records(tmp).values(), reverse=True):
+                print(f'Deleting record ==> {main_content[i]}')
+                main_content.pop(i)
+            f_out.write(''.join(main_content))
+
+    data = dict()
+    for filename in root_prefix.glob('*.json'):
+        with open(filename) as infile:
+            data.update(json.load(infile))
+    with open(f'{root_prefix}/data.json', 'w') as outfile:
+        json.dump(data, outfile, sort_keys=True)
+
+
+def test(root_prefix: str, model: str, shape: Tuple[int, int] = (256, 256)):
+    f = Path('data/datasets_for_testing_part/manual_from_google/pins_Amanda Crew')
+    o = Path('data/datasets_for_testing_part/manual_from_google/filtered')
+
+    creates_folders([f.name], o)
+
+    before = list(f.glob('*'))
+
+    skipped_img = parallelize_upd(
+        get_and_save_faces_from_image, np.array(before), {'shape': shape, 'out': o, 'model': model}
+    )
+
+    with open(f'{root_prefix}/not_found_faces_{model}_test.txt', 'a') as f:
+        for item in skipped_img:
+            print(item, file=f)
+
+    count_after = len(list(o.glob("*/*")))
+    print(
+        f'Count of images before 1 stage extract faces is {len(before)}\n'
+        f'Count of images after 1 stage extract faces is {count_after}'
+    )
+
+
+def resize_img(input_folder: list, shape: Tuple[int, int], root: Path):
+    for f in tqdm(input_folder, total=len(input_folder), desc=f'Processing folders ({current_process().name})...'):
+        key = Path(f'{root.parent}/resized_{root.name}/{f.name}')
+        key.mkdir(parents=True, exist_ok=True)
+        for p in f.glob('*'):
+            image = Image.open(p).resize(shape)
+            image.save(key / Path(p.name))
+    print('Resizing is done!')
+
+
+def launch_resize(input_folder: Path, shape: Tuple[int, int]):
+    l = list(input_folder.glob('*'))
+    parallelize(resize_img, np.array(l), {'shape': shape, 'root': input_folder})
+
+
+def select_images(input_folder: list, root: Path, folder_suitable_img: Path):
+    for f in tqdm(input_folder, total=len(input_folder), desc=f'Processing folders ({current_process().name})...'):
+        key = Path(f'{root}/{f.name}')
+        key.mkdir(parents=True, exist_ok=True)
+        for p in random.sample(list((folder_suitable_img / Path(f.name)).glob('*')), 25):
+            shutil.copy(f / Path(p.name), key / Path(p.name))
+    print('Selecting images are done!')
+
+
+def launch_selecting_images(input_folder: Path):
+    folder_suitable_img = Path('data/datasets_for_testing_part/mask_cnn_standard_all_unique_parsed_images')
+    o = Path('data/datasets_for_testing_part/final_test_dataset')
+    l = list(input_folder.glob('*'))
+    parallelize(select_images, np.array(l), {'root': o, 'folder_suitable_img': folder_suitable_img})
+
+
 def main():
     os.chdir(sys.path[-1])
     root = 'data/datasets_for_testing_part'
 
     # stage_0_standardise(root)
-    stage_1_filter_and_faces(root, 'hog')
+    # stage_1_filter_and_faces(root, 'hog')
     # stage_1_filter_and_faces(root, 'cnn')
+
+    # test(root, 'hog')
+
+    # stage_2_find_duplicates(Path(root))
+
+    # launch_resize(Path('data/input/105_classes_pins_dataset'), (256, 256))
+
+    # stage_3_standardise(root)
+
+    launch_selecting_images(Path('data/datasets_for_testing_part/standard_all_unique_parsed_images'))
 
 
 if __name__ == '__main__':
--- preprocessing_to_photos_with_masks.py
+++ preprocessing_to_photos_with_masks.py
@@ -1,7 +1,7 @@
 from utils import *
 
 
-def main():
+def train_dataset():
     cnn_filtered_photos_folder = Path('data/input/cnn_filtered_standardized_with_normalized_105_pins')
     hog_filtered_photos_folder = Path('data/input/filtered_standardized_with_normalized_105_pins')
 
@@ -26,6 +26,40 @@ def main():
         }
 
         parallelize(processing_photos, np.array(filtered_dataset.imgs), params)
+
+
+def test_dataset():
+    # cnn_filtered_photos_folder = Path('data/input/cnn_filtered_standardized_with_normalized_105_pins')
+    filtered_photos_folder = Path('data/datasets_for_testing_part/standard_all_unique_parsed_images')
+
+    # mask_cnn_filtered_photos_folder = Path('data/input/masks_cnn_filtered_standardized_with_normalized_105_pins')
+    mask_filtered_photos_folder = Path('data/datasets_for_testing_part/mask_cnn_standard_all_unique_parsed_images')
+
+    t = [('cnn', filtered_photos_folder, mask_filtered_photos_folder)]
+
+    # triple = (
+    #     ('cnn', filtered_photos_folder, mask_filtered_photos_folder),
+    #     ('hog', hog_filtered_photos_folder, mask_hog_filtered_photos_folder)
+    # )
+
+    tqdm_batch_inputs = tqdm(t, total=len(t))
+    for type_model, input_photos_folder, output_photos_folder in tqdm_batch_inputs:
+        filtered_dataset = ImageFolder(input_photos_folder.as_posix(), transform=T.ToTensor())
+
+        tqdm_batch_inputs.set_description(f'Filtered by {type_model}')
+
+        params = {
+            'classes': filtered_dataset.classes,
+            'output_photos_folder': output_photos_folder,
+            'type_model': type_model
+        }
+
+        parallelize(processing_photos, np.array(filtered_dataset.imgs), params)
+
+
+def main():
+    # train_dataset()
+    test_dataset()
 
 
 if __name__ == '__main__':
